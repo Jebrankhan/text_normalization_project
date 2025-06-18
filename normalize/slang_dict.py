bfcf3d9a7415cc7faef46e3c4ab50e2d61beaead
Jebrankhan <30473169+Jebrankhan@users.noreply.github.com>
2025-06-18 10:27:36 +0900

    Update slang_dict.py

--- normalize/slang_dict.py
+++ normalize/slang_dict.py
@@ -2,5 +2,13 @@
 slang_dict = {
     "u": "you", "ur": "your", "gr8": "great", "b4": "before",
     "luv": "love", "4ever": "forever", "omg": "oh my god", "plz": "please",
-    "idk": "i don't know", "smh": "shaking my head", "btw": "by the way"
+    "idk": "i don't know", "smh": "shaking my head", "btw": "by the way",
+    "hbd": "happy birthday", "gn": "good night", "gd": "good", "mrng": "morning",
+    "gd mrng": "good morning", "gud mrng": "good morning", "gud nyt": "good night"
+}
+
+contractions_dict = {
+    "can't": "cannot", "won't": "will not", "i'm": "i am", "it's": "it is",
+    "he's": "he is", "she's": "she is", "they're": "they are", "we're": "we are",
+    "didn't": "did not", "don't": "do not", "doesn't": "does not", "wasn't": "was not"
 }
